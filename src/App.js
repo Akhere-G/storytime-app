9dd05ea5d508fab202b2c0473a960e6148a6030f
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-28 22:24:06 +0000

    Styles to header, basic components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import storytimeLogo from "./images/storytimeLogo.png";
+import { Form, Posts } from "./components";
+import useStyles from "./styles";
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar className={classes.appBar} position='static' color='inherit'>
+        <Typography className={classes.heading} variant='h2' align='center'>
+          Storytime
+        </Typography>
+        <img
+          className={classes.image}
+          src={storytimeLogo}
+          alt='storytime'
+          height='60'
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='stretch'
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Post from "./Post/Post";
+const Posts = () => {
+  return (
+    <ul>
+      <Post />
+      <Post />
+      <Post />
+    </ul>
+  );
+};
+
+export default Posts;
