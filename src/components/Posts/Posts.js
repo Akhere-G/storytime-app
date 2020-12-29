e1076e297509e79dcfbab8a2e1e4712bfc90d7f7
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-29 04:50:44 +0000

    added redux dev tools

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Grid, CircularProgress } from "@material-ui/core";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 import { useSelector } from "react-redux";
@@ -8,12 +8,22 @@ const Posts = () => {
   const posts = useSelector(state => state.posts);
   const classes = useStyles();
   console.log(posts);
+
+  if (!posts.length) {
+    return <CircularProgress />;
+  }
+
   return (
-    <ul>
-      <Post />
-      <Post />
-      <Post />
-    </ul>
+    <Grid
+      className={classes.container}
+      container
+      alignItems='stretch'
+      spacing={3}
+    >
+      {posts.map(post => {
+        return <Post key={post._id} {...post} />;
+      })}
+    </Grid>
   );
 };
 
